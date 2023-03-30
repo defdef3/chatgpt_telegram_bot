a50e68f491a28ab60fede48673844a682f6833a8
defdef3 <126761539+defdef3@users.noreply.github.com>
2023-03-30 09:10:17 +0300

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -154,10 +154,13 @@ async def message_handle(update: Update, context: CallbackContext, message=None,
                 await update.message.reply_text(f"Starting new dialog due to timeout (<b>{openai_utils.CHAT_MODES[chat_mode]['name']}</b> mode) ✅", parse_mode=ParseMode.HTML)
         db.set_user_attribute(user_id, "last_interaction", datetime.now())
 
-        # send typing action
-        await update.message.chat.send_action(action="typing")
-
         try:
+            # send placeholder message to user
+            placeholder_message = await update.message.reply_text("...")
+
+            # send typing action
+            await update.message.chat.send_action(action="typing")
+
             message = message or update.message.text
 
             current_model = db.get_user_attribute(user_id, "current_model")
@@ -182,45 +185,32 @@ async def message_handle(update: Update, context: CallbackContext, message=None,
 
                 gen = fake_gen()
 
-            # send message to user
             prev_answer = ""
-            i = -1
             async for gen_item in gen:
-                i += 1
-
                 status = gen_item[0]
                 if status == "not_finished":
                     status, answer = gen_item
                 elif status == "finished":
                     status, answer, (n_input_tokens, n_output_tokens), n_first_dialog_messages_removed = gen_item
                 else:
-                    raise ValueError(f"Streaming status {status} is unknown")
+                    raise ValueError(f"Состояние потокового вещания {status} неизвестно")
 
                 answer = answer[:4096]  # telegram message limit
-                if i == 0:  # send first message (then it'll be edited if message streaming is enabled)
-                    try:                    
-                        sent_message = await update.message.reply_text(answer, parse_mode=parse_mode)
-                    except telegram.error.BadRequest as e:
-                        if str(e).startswith("Сообщение должно быть непустым"):  # first answer chunk from openai was empty
-                            i = -1  # try again to send first message
-                            continue
-                        else:
-                            sent_message = await update.message.reply_text(answer)
-                else:  # edit sent message
-                    # update only when 100 new symbols are ready
-                    if abs(len(answer) - len(prev_answer)) < 100 and status != "finished":
+
+                # update only when 100 new symbols are ready
+                if abs(len(answer) - len(prev_answer)) < 100 and status != "finished":
+                    continue
+
+                try:
+                    await context.bot.edit_message_text(answer, chat_id=placeholder_message.chat_id, message_id=placeholder_message.message_id, parse_mode=parse_mode)
+                except telegram.error.BadRequest as e:
+                    if str(e).startswith("Сообщение не изменено"):
                         continue
+                    else:
+                        await context.bot.edit_message_text(answer, chat_id=placeholder_message.chat_id, message_id=placeholder_message.message_id)
 
-                    try:                    
-                        await context.bot.edit_message_text(answer, chat_id=sent_message.chat_id, message_id=sent_message.message_id, parse_mode=parse_mode)
-                    except telegram.error.BadRequest as e:
-                        if str(e).startswith("Message is not modified"):
-                            continue
-                        else:
-                            await context.bot.edit_message_text(answer, chat_id=sent_message.chat_id, message_id=sent_message.message_id)
+                await asyncio.sleep(0.01)  # wait a bit to avoid flooding
 
-                    await asyncio.sleep(0.01)  # wait a bit to avoid flooding
-                    
                 prev_answer = answer
 
             # update user data
@@ -231,7 +221,7 @@ async def message_handle(update: Update, context: CallbackContext, message=None,
                 dialog_id=None
             )
 
-            db.update_n_used_tokens(user_id, current_model, n_input_tokens, n_output_tokens) 
+            db.update_n_used_tokens(user_id, current_model, n_input_tokens, n_output_tokens)
         except Exception as e:
             error_text = f"Что-то пошло не так во время завершения. Причина: {e}"
             logger.error(error_text)
@@ -245,8 +235,8 @@ async def message_handle(update: Update, context: CallbackContext, message=None,
             else:
                 text = f"✍️ <i>Note:</i> Your current dialog is too long, so <b>{n_first_dialog_messages_removed} first messages</b> were removed from the context.\n Send /new command to start new dialog"
             await update.message.reply_text(text, parse_mode=ParseMode.HTML)
-
-
+            
+            
 async def is_previous_message_not_answered_yet(update: Update, context: CallbackContext):
     await register_user_if_not_exists(update, context, update.message.from_user)
 
@@ -315,13 +305,7 @@ async def image_generation_handle(update: Update, context: CallbackContext):
     reply_markup = InlineKeyboardMarkup(keyboard)
     
     # send image with inline keyboard
-    await update.effective_chat.send_photo(
-        photo=image_url,
-        caption=caption,
-        reply_markup=reply_markup,
-        timeout=30
-    )
-    
+    await update.message.reply_photo(image_url, caption=caption, reply_markup=reply_markup)
     return ConversationHandler.END
 
 async def button_click_handle(update: Update, context: CallbackContext):
